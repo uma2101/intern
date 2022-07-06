404a06a8cf93e47f82b5c3f980ba7c8f03e952fd
uma <108787507+uma2101@users.noreply.github.com>
2022-07-06 12:45:05 +0530

    Create inventory.java

---
+++ inventory.java
@@ -0,0 +1,15 @@
+InventoryItem.java:
+package Home_Inventory_Manager;
+    public class InventoryItem
+    {
+        public String description;
+        public String location;
+        public boolean marked;
+        public String serialNumber;
+        public String purchasePrice;
+        public String purchaseDate;
+
+      public String purchaseLocation;       
+      public String note;
+       public String photoFile;
+   }
